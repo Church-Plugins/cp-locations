875bbbfab7b9f10c89141c45af1da9bb2172de60
Tanner Moushey <tanner@missionlab.dev>
2022-05-18 15:11:04 -0500

    update popup styling

--- templates/map/popup.php
+++ templates/map/popup.php
@@ -15,8 +15,8 @@ $data = $location->get_api_data( false );
 ?>
 
 <div class="cploc-map-popup">
-	<h4 class="cploc-map-popup--title"><?php echo $data['title']; ?></h4>
 	<div class="cploc-map-popup--thumb"><?php echo get_the_post_thumbnail( $location->post->ID, 'large' ); ?></div>
+	<h4 class="cploc-map-popup--title"><?php echo $data['title']; ?></h4>
 	<div class="cploc-map-popup--address"><?php echo wpautop( $data['address'] ); ?></div>
 	<div class="cploc-map-popup--times"></div>
 	<div class="cploc-map-popup--cta"><a href="<?php echo get_the_permalink( $location->post->ID ); ?>" class="cp-button">Learn More</a></div>
